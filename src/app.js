72599a31cd4bde07a4394cd8ee65dd2dfb38b76a
Anson <ansfoong@gmail.com>
2020-08-31 05:24:39 -0400

    test

--- src/app.js
+++ src/app.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.json({ msg: 'Hello!' });
 });
 
-app.get('/hello', (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.json({ msg: 'Hello!', status: 200 });
 });
 
-app.get('/dashboard', (req, res) => {
+app.get('/api/dashboard', (req, res) => {
   res.json({ msg: 'Hello!', status: 201 });
 });
 
